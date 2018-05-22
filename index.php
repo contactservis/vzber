e3c0dad47d0980054230b80c434aa684491ea474
Igor Solowyov <solovyow_iv@nfksber.ru>
2018-05-22 10:21:07 +0300

    Верстка первой главной страницы

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Зберополис</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/css.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <!--Шапка с телефоном-->
+        <div class="tel_container" style="height: 35px; background-color: #f2f2f2;">
+                <div class="container">
+                        <div class="row">
+                            <div class="col-xl-8"></div>    
+                            <div class="col-xl-4 align-middle">
+                                <a href="#" class="float-right">Бесплатно по России 8 (800) 000 00 00</a>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        <!--навигационная панель-->
+        <div class="container">
+            <div class="row" style="padding: 22px 0;">                
+                    <div class="col-xl-4">                            
+                        <img src="img/logo.png">                        
+                    </div>
+                    <div class="col-xl-8 main_menu_box">
+                        <ul class="nav main_menu">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">ГЛАВНАЯ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">О ПРОЕКТЕ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ИНВЕСТОРУ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ЭМИТЕНТУ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ПОМОЩЬ</a>
+                            </li>
+                        </ul>
+                        <button type="button" class="btn btn-outline-secondary float-right">ВХОД/РЕГИСТРАЦИЯ</button>
+                    </div>                
+            </div>
+        </div>
+        <!--Большой рекламный слайдер-->
+        <div class="main_slayder">
+            <div style="overflow: hidden; width: 100%; text-align: center;">
+                <img src="img/big_baner2.jpg" alt="инвестиционный сервис Зберополис"/>                
+            </div>
+        </div>
+        <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <h2 class="text-center head_block">ПРЕИМУЩЕСТВА И УСЛОВИЯ</h2>
+                    </div>
+                </div>
+                <div class="row">
+                        <div class="col-xl-12" >
+                            <div class="pic_box">
+                                <div class="active_cir float-left" data="f"></div>
+                                <div class="float-left" data="f"></div>
+                                <div class="float-left" data="f"></div>
+                                <div class="float-left" data="f"></div>                                                              
+                            </div>
+                        </div>
+                </div>
+        </div>
+        <!--Блок преимущества-->
+        <div class="container blok_pre">
+            <div class="row">
+                <div class="col-xl-6">
+                    <div style="border: 1px solid #dddddd; height: 167px;border-radius: 5px;">
+                        <div class="row">
+                            <div class="col-xl-4 col-sm-4">
+                                <img src="img/inv_ico_pre.png">
+                            </div>    
+                            <div class="col-xl-8 col-sm-8 pre_text">
+                                    <h3>ИНВЕСТРОМ МОЖЕТ СТАТЬ КАЖДЫЙ</h3>
+                                    <p>минимальная сумма инвестирования 10 000 руб.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                        <div style="border: 1px solid #dddddd; height: 167px;border-radius: 5px;">
+                            <div class="row">
+                                <div class="col-xl-4 col-sm-4">
+                                    <img src="img/fin_ico_pre.png">
+                                </div>    
+                                <div class="col-xl-8 col-sm-8 pre_text">
+                                    <h3>БЫСТРОЕ ФИНАНСИРОВАНИЕ БЕЗ ЗАЛОГА</h3>
+                                        <p>Сроки привличения средств от 1 месяца.</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <!--Блок о сервисе-->
+        <div class="container blok_pre">
+            <div class="row">
+                <div class="col-xl-12 col-sm-12">
+                    <div class="rounded button_os"><span class="bt_sr_active">О сервисе</span><span>Особенности сервиса</span></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-6 col-sm-1">
+                    <h2>ZБЕРОПОЛИС</h2>
+                    <h3 style="margin-top: 13px;">ОНЛАЙН-ПЛОЩАДКА ДЛЯ ПРИВЛЕЧЕНИЯ ИНВЕСТИЦИЙ В БИЗНЕС ПРОЕКТЫ</h3>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 30px;">
+                <div class="col-xl-6 col-sm-1">
+                    <img class="slayd rounded" src="img/sld_oserv.jpg" alt="Зберополис инвестиционный сервис" />
+                </div>
+                <div class="col-xl-6 col-sm-1">
+                    <p>ZBEROPOLIS – это сервис, который предоставляет людям возможность вкладывать свои сбережения в различные направления: от предприятий малого и среднего бизнеса под ставку свыше 30% годовых до государственных облигаций, дающих высочайший уровень защиты капитала.</p>
+                    <p>Это также механизм, позволяющий небольшим компаниям быстро и эффективно привлекать финансирование для своих проектов.</p>
+                    <p>Инвесторами могут быть физические или юридические лица, получателями финансирования (эмитентами) – коммерческие организации или индивидуальные предприниматели, зарегистрированные на территории Российской Федерации. Посредником между инвесторами и эмитентами выступает брокер – профессиональный участник рынка ценных бумаг.</p>
+                </div>
+            </div>
+        </div>
+        <!--Блок с проектами-->
+        <div class="container" style="margin-bottom:100px;">
+                <div class="row">
+                    <div class="col-xl-12 col-sm-12">
+                        <h2 class="text-center head_block">ПРОЕКТЫ</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-12 col-sm-12">
+                        <div class="pic_box">
+                            <div class="active_cir float-left" data="f"></div>
+                            <div class="float-left" data="f"></div>
+                            <div class="float-left" data="f"></div>
+                            <div class="float-left" data="f"></div>                                                              
+                        </div>
+                    </div>
+                </div>
+                <div class="row main_list_proekt">
+                    <div class="col-xl-12 col-sm-12">
+                        <?php 
+                            $arr = array(
+                                array('КОФЕЙНЯ', 'img/proekt1.jpg'),
+                                array('Перерабатывающий комплекс', 'img/proekt2.jpg'),
+                                array('Добыча песка на речном карьере', 'img/proekt3.jpg'),
+                                array('Сервис электронных платежей', 'img/proekt4.jpg')
+                            );
+                            foreach($arr as $item){
+                        ?>
+                            <div class="card">
+                                    <img class="card-img-top" src="<?=$item[1]?>" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?=$item[0]?></h5>
+                                      <div>
+                                        <span style="margin-right: 8px;">Срок погашения<p>21.12.2018</p></span>
+                                        <span style="margin-right: 8px;"><img src="img/procent.png"/></span>
+                                        <span>Ставка: <p>7,52%</p></span>
+                                      </div>
+                                      <a class="btn btn-block" href="#" role="button">Инвестировать</a>                                   
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+        </div>
+    <!--Блок схема работы-->
+    <div class="container">
+        <div class="row" style="margin-top: 30px;">
+                <div class="col-xl-6 col-sm-1">
+                    <img class="slayd rounded" src="img/sld_oserv2.jpg" alt="Зберополис инвестиционный сервис" />
+                </div>
+                <div class="col-xl-6 col-sm-1">
+                    <h2>Для инвесторов</h2>
+                    <h3>как вложить деньги</h3>
+                    <p>ZBEROPOLIS – это сервис, который предоставляет людям возможность вкладывать свои сбережения в различные направления: от предприятий малого и среднего бизнеса под ставку свыше 30% годовых до государственных облигаций, дающих высочайший уровень защиты капитала.</p>
+                    <p>Это также механизм, позволяющий небольшим компаниям быстро и эффективно привлекать финансирование для своих проектов.</p>
+                    <p>Инвесторами могут быть физические или юридические лица, получателями финансирования (эмитентами) – коммерческие организации или индивидуальные предприниматели, зарегистрированные на территории Российской Федерации. Посредником между инвесторами и эмитентами выступает брокер – профессиональный участник рынка ценных бумаг.</p>
+                </div>
+        </div>
+    </div>
+    <!--Блок регистрация/авторизация-->
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+    <!--Блок вопросы-->
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+    <!--Подвал-->
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+    </body>
+</html>
